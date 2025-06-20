315856bc64e0d7336030dcbcad3d7dd4f9719563
Artem Polianskyi <artem.polyanskiy.dnepr@gmail.com>
2025-06-20 20:53:31 +0300

    Added new sounds, health text UI, blood screen and game over screen

--- player.py
+++ player.py
@@ -10,6 +10,33 @@ class Player:
         self.angle = PLAYER_ANGLE
         self.rel = None
         self.shot = False
+        self.health = PLAYER_MAX_HEALTH
+        self.rel = 0
+        self.health_recovery_delay = 700
+        self.time_prev = pygame.time.get_ticks()
+
+    def recover_health(self):
+        if self.check_health_recovery_delay() and self.health < PLAYER_MAX_HEALTH:
+            self.health += 1
+
+    def check_health_recovery_delay(self):
+        time_now = pygame.time.get_ticks()
+        if time_now - self.time_prev > self.health_recovery_delay:
+            self.time_prev = time_now
+            return True
+
+    def check_game_over(self):
+        if self.health < 1:
+            self.game.object_renderer.game_over()
+            pygame.display.flip()
+            pygame.time.delay(1500)
+            self.game.new_game()
+
+    def get_damage(self, damage):
+        self.health -= damage
+        self.game.object_renderer.player_damage()
+        self.game.sound.player_pain.play()
+        self.check_game_over()
 
     def single_fire_event(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN:
@@ -71,6 +98,7 @@ class Player:
     def update(self):
         self.movement()
         self.mouse_control()
+        self.recover_health()
 
     @property
     def pos(self):
--- sound.py
+++ sound.py
@@ -9,4 +9,6 @@ class Sound:
         self.shotgun = pygame.mixer.Sound(self.path + 'shotgun.wav')
         self.npc_pain = pygame.mixer.Sound(self.path + 'npc_pain.wav')
         self.npc_death = pygame.mixer.Sound(self.path + 'npc_death.wav')
+        self.npc_attack = pygame.mixer.Sound(self.path + 'npc_attack.wav')
+        self.player_pain = pygame.mixer.Sound(self.path + 'player_pain.wav')
         self.shotgun.set_volume(0.5)
